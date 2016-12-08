faedae66c4d1088433071c19215ee35eb546bae0
Madars Bitenieks <madza@madzathemes.com>
2016-12-08 22:07:00 +0200

    fix

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
 <div class="footer-wrap">
-
+		<?php $optionz = get_option("magazin_theme_options");
+		if (!empty($optionz['article_ad_bottom'])) {  ?>
+			<div class="advertise text-center">
+				<?php echo html_entity_decode($optionz['article_ad_bottom']); ?>
+			</div>
+		<?php } ?>
 	<?php $option = get_option("infowazz_theme_options"); ?>
 	<?php if(!empty($option['footer_page'])){ ?>
 		<?php $footer_page = $option['footer_page']; ?>
@@ -8,12 +13,6 @@
 				<?php the_content(); ?>
 			</div>
 		<?php endwhile; wp_reset_postdata(); ?>
-	<?php } ?>
-	<?php $optionz = get_option("magazin_theme_options");
-	if (!empty($optionz['article_ad_bottom'])) {  ?>
-		<div class="advertise text-center">
-			<?php echo html_entity_decode($optionz['article_ad_bottom']); ?>
-		</div>
 	<?php } ?>
 	<?php if(!empty($option['footer_top']) or !empty($option['footer_bottom'])){ ?>
 		<div class="footer">
